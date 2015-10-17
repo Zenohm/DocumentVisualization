fa405d3d15dffc249ba3f396e1bd8a61e90d9908
Chris Bellis <chrisfwin@gmail.com>
2015-10-16 20:25:07 -0400

    Adding the flex analyzer

--- lucene-index/src/main/java/searcher/analyzer/FlexAnalyzerBuilder.java
+++ lucene-index/src/main/java/searcher/analyzer/FlexAnalyzerBuilder.java
@@ -2,10 +2,15 @@ package searcher.analyzer;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.TokenFilter;
+import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.Tokenizer;
 import org.apache.lucene.analysis.core.WhitespaceTokenizer;
 import org.apache.lucene.analysis.standard.StandardFilter;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -25,10 +30,19 @@ public class FlexAnalyzerBuilder {
     }
 
     // TODO: Think this through some more!
-
     private FlexAnalyzerBuilder() {
         tokenizer = new WhitespaceTokenizer();
         filters = new ArrayList<>();
+    }
+
+    public FlexAnalyzerBuilder setTokenizer(Tokenizer tokenizer) {
+        this.tokenizer = tokenizer;
+        return this;
+    }
+
+    public FlexAnalyzerBuilder addFilter(Class<? extends TokenFilter> filter) {
+        filters.add(filter);
+        return this;
     }
 
     public FlexAnalyzer build() {
@@ -36,17 +50,34 @@ public class FlexAnalyzerBuilder {
         return analyzer;
     }
 
-
-
     class FlexAnalyzer extends Analyzer {
+        Tokenizer tokenizer;
         List<Class<? extends TokenFilter>> filters;
 
         public FlexAnalyzer(List<Class<? extends TokenFilter>> filters, Tokenizer tokenizer) {
             filters = new ArrayList<>(filters);
+            this.tokenizer = tokenizer;
         }
         @Override
         protected TokenStreamComponents createComponents(String s) {
-            throw new UnsupportedOperationException("Not implemented yet");
+            StringReader reader = new StringReader(s);
+            try{
+                tokenizer.setReader(reader);
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+            TokenStream filter = new StandardFilter(tokenizer);
+            for(Class<? extends TokenFilter> f : filters) {
+                try {
+                    filter = f.getConstructor().newInstance(filter);
+                } catch (InstantiationException |
+                         IllegalAccessException |
+                         NoSuchMethodException  |
+                         InvocationTargetException e) {
+                    System.err.println("Could not create an instance of " + f.getName());
+                }
+            }
+            return new TokenStreamComponents(tokenizer, filter);
         }
     }
 
