3b3e78ffee6eb99cd01f570506cf4223be79c4cc
Chris Bellis <chrisfwin@gmail.com>
2015-11-28 15:28:10 -0500

    Making PDF retriever work. Adding metadata to the multiTermGraph and completing the first-level visualization

--- vis-webapp/src/main/webapp/js/multiTermGraph.js
+++ vis-webapp/src/main/webapp/js/multiTermGraph.js
@@ -29,6 +29,9 @@
  */
 
 function forceChart() {
+    // "Constant" Variables
+    var FIXED_NODE_SIZE = 50;
+
     // "Class" Variables
     var width = 400,
         height = 400,
@@ -37,6 +40,7 @@ function forceChart() {
         color = d3.scale.category20(),
         link = 0,
         node = 0,
+        text = 0,
         quadTree = 0,
         graph = 0,
         me = 0;
@@ -55,16 +59,18 @@ function forceChart() {
 
             force = d3.layout.force()
                 .charge(-150)
-                .linkDistance(function(d){return d.link_power;})
                 .linkStrength(function(d){return d.link_power;})
                 .size([width, height]);
 
-            svg = d3.select(this).append("svg")// TODO: Probably need to add code to remove this if it already exists
-                .attr("width", width)
-                .attr("height", height);
+            // Check if the SVG exists, if it doesn't create it
+            svg = d3.select(this).selectAll("svg");
+            if(svg.empty()){
+                d3.select(this).append("svg").attr("width", width).attr("height", height);
+            }
 
             quadTree = d3.geom.quadtree(d.nodes);
 
+            // Add the data, start the sim.
             force.nodes(d.nodes)
                 .links(d.links)
                 .start();
@@ -82,23 +88,36 @@ function forceChart() {
             node = svg.selectAll(".node")
                 .data(d.nodes);
 
-            console.log(d.nodes);
-
+            // Adding the nodes
             node.enter().append("circle")
                 .attr("class", "node")
                 .attr("r", function(d){
                     if(d.fixed){
-                        return 40;
+                        return FIXED_NODE_SIZE;
                     }else{
                         return d.size;
                     }
                 })
                 .attr("radius", function(d){return d.r;})
                 .style("fill", function (d) {
-                    console.log(d.color);
                     return d3.rgb(d.color);
+                })
+                .on("click", function(d){
+                    displayDocument(d.docId);
                 });
 
+            text = svg.selectAll("text").data(d.nodes.filter(function(d){return d.fixed;}));
+            text.enter()
+                .append("text").text(function(d){
+                    return d.name;
+                })
+                .attr("font-family", "sans-serif")
+                .attr("font-size", "12px")
+                .attr("fill", "black")
+                .attr("text-anchor", "middle");
+
+
+            // If a node is removed, remove it from the sim.
             node.exit().remove();
 
             svg.selectAll(".node").data(d.nodes).exit().remove();
@@ -168,6 +187,12 @@ function forceChart() {
                 }
         });
 
+        text.attr("x", function(d){
+            return d.x;
+        }).attr("y", function(d){
+            return d.y;
+        })
+
         link.attr("x1", function (d) {
                 return d.source.x;
             })
@@ -180,15 +205,11 @@ function forceChart() {
             .attr("y2", function (d) {
                 return d.target.y;
             });
-
-
-
-
     }
 
 
     var clusterPadding = 20, // separation between different-color nodes
-        maxRadius = 24,
+        maxRadius = 50,
         padding = 1.5;
 
     /**
